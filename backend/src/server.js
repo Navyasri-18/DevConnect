0be3d4b912ddb612d3299fdc8a76862f34bf45a5
Kamble Navyasri <navyak1585@gmail.com>
2025-11-05 19:56:53 +0530

    DB and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,37 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
 import path from 'path';
+import { connectDB } from './lib/db.js';
 
 const app = express()
 const __dirname = path.resolve();
 
 app.get('/health', (req, res) => {
-  res.status(200).json({msg : "api is up and running"});
+  res.status(200).json({ msg: "api is up and running" });
 })
 
 app.get('/books', (req, res) => {
-  res.status(200).json({msg : "this is the books endpoint"});
+  res.status(200).json({ msg: "this is the books endpoint" });
 })
 
 //Make our app ready for deployment
 if (ENV.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
+
   app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   })
 }
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server running on port 3000`)
-})
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log("Server running on port :", ENV.PORT);
+    });
+  } catch (error) {
+    console.error("❌ Failed to start server : ", error);
+  }
+};
+startServer();
