897280e0e6d45f9cb52a48a954973232fabc07d9
Kamble Navyasri <navyak1585@gmail.com>
2025-11-05 14:29:52 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,27 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
+import path from 'path';
 
 const app = express()
+const __dirname = path.resolve();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/health', (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 })
+
+app.get('/books', (req, res) => {
+  res.status(200).json({ msg: "this is the books endpoint" });
+})
+
+//Make our app ready for deployment
+if (ENV.NODE_ENV === 'production') {
+  // in backend/index.js
+  app.use(express.static('../frontend/dist'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'))
+  })
+
+}
 
 app.listen(ENV.PORT, () => {
   console.log(`Server running on http://localhost:3000`)
